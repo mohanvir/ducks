e0ae60635bb88444e91a60c985fcd9c8bcf68c2e
Mohanvir Gill <mohanvir@Mohanvirs-MBP.attlocal.net>
2022-06-28 19:27:19 -0700

    Added Metrics

--- python/app.py
+++ python/app.py
@@ -2,9 +2,11 @@ def hello_ducks():
     return "hello_ducks"
 
 def hello_humans():
+    human_count = human_count +1
     return "hello humans"
 
 def hello_class():
+    class_count = class_count + 1
     return "hello class"
 
 if __name__ == "__main__":
---
+++ python/metric_tracker.py
@@ -0,0 +1,3 @@
+human_count = 0
+
+class_count = 0
