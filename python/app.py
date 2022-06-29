b062c12bb5a07a5235ca7d5328d140d9b21f9f31
Mohanvir Gill <mohanvir@Mohanvirs-MBP.attlocal.net>
2022-06-28 19:23:53 -0700

    new structure

---
+++ python/app.py
@@ -0,0 +1,12 @@
+def hello_ducks():
+    return "hello_ducks"
+
+def hello_humans():
+    return "hello humans"
+
+def hello_class():
+    return "hello class"
+
+if __name__ == "__main__":
+    print(hello_ducks())
+    print(hello_humans())
