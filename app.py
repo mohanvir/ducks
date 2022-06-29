1ecf296ab7b62ebbf9773743832b16e94b95b455
Ryndine <rcmangeno@gmail.com>
2022-06-28 19:27:08 -0700

    adding ducks names

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-def hello_ducks():
-    return "hello_ducks"
+duck_names = ["Quacks", "Waddles", "Steve"]
+
+def hello_ducks(ducks):
+    names = []
+    print("Hello Ducks")
+    for i in ducks:
+        names.append(i)
+    return names
 
 def hello_humans():
     return "hello humans"
 
-<<<<<<< HEAD
 def hello_class():
     return "hello class"
-=======
 
 if __name__ == "__main__":
     print(hello_ducks())
     print(hello_humans())
->>>>>>> 724f009868dba1647d6841fea915b51e2fd990f3
